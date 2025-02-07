f0dbf1ea4c0034702e2476b40a7d5cb39b201a6d
user <chocovy82@gmail.com>
2025-02-08 07:42:52 +0900

    update HybridEngine2

--- storage/corpus/program_20250208064552_81B5C40F-709F-4754-B671-28151EC4D908.js
+++
@@ -1,5 +0,0 @@
-// Minimizing 9A2B6E5F-30F2-4B05-89E3-233E19B12A35
-new BigInt64Array(67);
-new Uint16Array(1024);
-gc();
-// Program is interesting due to new coverage: 56 newly discovered edges in the CFG of the target
--- storage/corpus/program_20250208064645_8E2F5803-AA9E-42EE-967A-8ADE02152B01.js
+++
@@ -1,8 +0,0 @@
-// Minimizing 131212F3-4037-449D-BDD2-9C3CDDAC1D16
-const v0 = [-2.7629137379852313,0.5350155291449148,2.2250738585072014e-308,-1000000.0,0.7883988340622813,-1.7976931348623157e+308];
-v0.length = 2;
-for (let v2 = 0; v2 < 5; v2++) {
-    new Proxy(v0, {});
-}
-gc();
-// Program is interesting due to new coverage: 16 newly discovered edges in the CFG of the target
